6479e3c85914ceb81e4426285631878c4ae1751d
Joe Maples <joe.maples@valtech.com>
2024-04-28 16:43:09 -0400

    feat: Add deb-get for wifi and kernel updates, simplify printer setup

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -28,6 +28,11 @@ expire_passwds() {
 	passwd --expire root
 }
 
+deb-get_setup() {
+	curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+	cp -R /tmp/overlay/pkgs /etc/deb-get/99-local.d
+}
+
 wifi_driver_setup() {
 	cd /tmp
 	git clone https://github.com/lynxlikenation/aic8800.git
@@ -46,115 +51,26 @@ misc_root_setup() {
 	systemctl mask serial-getty@ttyS2.service
 }
 
-kiauh_setup() {
-	cd /home/$USER
-	git clone https://github.com/dw-0/kiauh
-}
-
-prepare_kiauh_env() {
-	cd /home/$USER
-	KIAUH_SRCDIR="${HOME}/kiauh"
-	source $KIAUH_SRCDIR/scripts/ui/general_ui.sh
-	source $KIAUH_SRCDIR/scripts/globals.sh
-	source $KIAUH_SRCDIR/scripts/utilities.sh
-	set_globals
-}
-
-klipper_setup() {
-	cd /home/$USER
-	source $KIAUH_SRCDIR/scripts/klipper.sh
-	run_klipper_setup 3 printer
-	/home/$USER/klippy-env/bin/pip install numpy
-}
-
-moonraker_setup() {
-	cd /home/$USER
-	source $KIAUH_SRCDIR/scripts/moonraker.sh
-	moonraker_setup 1
-}
-
-fluidd_setup() {
-	cd /home/$USER
-	source $KIAUH_SRCDIR/scripts/backup.sh
-	source $KIAUH_SRCDIR/scripts/moonraker.sh
-	source $KIAUH_SRCDIR/scripts/nginx.sh
-	source $KIAUH_SRCDIR/scripts/fluidd.sh
-	yes | install_fluidd
-}
-
-crowsnest_setup() {
-	cd /home/$USER
-	export CROWSNEST_UNATTENDED=1
-	git clone https://github.com/mainsail-crew/crowsnest
-	cd crowsnest
-	sudo CROWSNEST_UNATTENDED=1 make install
-}
-
-timelapse_setup() {
-	cd /home/$USER
-	git clone https://github.com/mainsail-crew/moonraker-timelapse.git
-	cd /home/$USER/moonraker-timelapse
-	yes | make install
-}
-
-auto_z_offset_setup() {
-	cd /home/$USER
-	git clone https://github.com/frap129/qidi_auto_z_offset
-	ln -s /home/$USER/qidi_auto_z_offset/auto_z_offset.py /home/$USER/klipper/klippy/extras/auto_z_offset.py
-}
-
-config_setup() {
-	cd /home/$USER
-	git clone https://github.com/frap129/openq1
-	cd openq1
-	./install.sh /home/$USER/printer_data/config
-
-	# Setup KAMP
-	cd /home/$USER
-	git clone https://github.com/kyleisah/Klipper-Adaptive-Meshing-Purging.git
-	ln -s /home/$USER/Klipper-Adaptive-Meshing-Purging/Configuration /home/$USER/printer_data/config/KAMP
-	cp /home/$USER/Klipper-Adaptive-Meshing-Purging/Configuration/KAMP_Settings.cfg /home/$USER/printer_data/config/KAMP_Settings.cfg
-}
-
-shaketune_setup() {
-	cd /home/$USER
-	git clone https://github.com/Frix-x/klippain-shaketune/ klippain_shaketune
-	cd klippain_shaketune
-	head -n -9 install.sh > tmp-install.sh
-	echo "setup_venv
-	link_extension" >> tmp-install.sh
-	source tmp-install.sh
-	rm tmp-install.sh
-}
-
-shellcmd_setup() {
-	cd /home/$USER
-	source $KIAUH_SRCDIR/scripts/gcode_shell_command.sh
-	yes n | install_gcode_shell_command
-}
-
-kiauh_setup_root() {
+openq1_setup_root() {
 	echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER
 	su $USER -c "/tmp/customize-image.sh"
 	sed -i "s/ NOPASSWD://g" /etc/sudoers.d/$USER
 }
 
+openq1_setup_user() {
+	cd /home/$USER
+	git clone https://github.com/frap129/OpenQ1
+	cd OpenQ1
+	./scripts/install.sh
+}
+
 if [[ "$(whoami)" == "root" ]]; then
 	user_setup
+	deb-get_setup
 	wifi_driver_setup
 	misc_root_setup
-	kiauh_setup_root
+	openq1_setup_root
 	#expire_passwds
 elif [[ "$(whoami)" == "$USER" ]]; then
-	kiauh_setup
-	prepare_kiauh_env
-	klipper_setup
-	moonraker_setup
-	fluidd_setup
-	crowsnest_setup
-	timelapse_setup
-	auto_z_offset_setup
-	config_setup
-	shellcmd_setup
-	shaketune_setup
+	openq1_setup_user
 fi
